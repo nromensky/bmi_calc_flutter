d5b2da310817b083bf5d829bc0f63b5686504e77
user <nromensky@gmail.com>
2021-03-11 18:46:10 +0300

    .

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -13,7 +13,8 @@ class InputPage extends StatefulWidget {
 
 class _InputPageState extends State<InputPage> {
   enumGenderButtons selectedGenderButton;
-  int selectedWeight = 165;
+  int selectedWeight = 65;
+  int selectedHeight = 165;
 
   @override
   Widget build(BuildContext context) {
@@ -107,29 +108,29 @@ class _InputPageState extends State<InputPage> {
                       mainAxisAlignment: MainAxisAlignment.center,
                       crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        Text(
-                          'HEIGHT',
-                        ),
+                        Text('HEIGHT'),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           crossAxisAlignment: CrossAxisAlignment.baseline,
                           textBaseline: TextBaseline.alphabetic,
                           children: [
                             Text(
-                              selectedWeight.toString(),
-                              textScaleFactor: 2.2,
-                              style: TextStyle(fontFamily: 'Fascinate'),
+                              selectedHeight.toString(),
+                              style: TextStyle(
+                                fontFamily: 'NovaMono',
+                                fontSize: 67,
+                              ),
                             ),
                             Text('cm')
                           ],
                         ),
                         Slider(
-                          value: selectedWeight.toDouble(),
+                          value: selectedHeight.toDouble(),
                           min: 100,
                           max: 240,
                           onChanged: (double value) {
                             setState(() {
-                              selectedWeight = value.round();
+                              selectedHeight = value.round();
                             });
                           },
                         )
@@ -140,8 +141,43 @@ class _InputPageState extends State<InputPage> {
 
             // *********** WEIGHT and AGE section ***********
 
-            Expanded(flex: 3, child: Row()),
-            Expanded(flex: 1, child: Row()),
+            Expanded(
+              flex: 3,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  TextButton(
+                    onPressed: null,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [Text('WEIGHT')],
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: null,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [Text('AGE')],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            // *********** CALCULATE button section ***********
+
+            Expanded(
+              flex: 1,
+              child: Container(
+                width: double.infinity,
+                padding: EdgeInsets.all(16),
+                color: Colors.red,
+                child: Text('CALCULATE'),
+              ),
+            ),
           ],
         ));
   }
